719306b2f64fc733012156c5ffb41af12d5f9500
emilyjryan <emilyryanmac@gmail.com>
2023-06-17 13:39:48 -0700

    created some globally scoped functions to be called in the for loop

--- js/script.js
+++ js/script.js
@@ -1,69 +1,99 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-// Initial console.log to ensure JS file is linked to hmtl:
-console.log('JS file loaded and running')
+  // Initial console.log to ensure JS file is linked to HTML:
+  console.log('JS file loaded and running')
 
-// Defining variables from html elements:
-const hlaA = document.getElementById('hla-a');
-const hlaB = document.getElementById('hla-b');
-const hlaC = document.getElementById('hla-c');
-const hlaDP = document.getElementById('hla-dp');
-const hlaDQ = document.getElementById('hla-dq');
-const hlaDR = document.getElementById('hla-dr');
-const other = document.getElementById('other');
+  // Defining variables from html elements:
+  const hlaA = document.getElementById('hla-a');
+  const hlaB = document.getElementById('hla-b');
+  const hlaC = document.getElementById('hla-c');
+  const hlaDP = document.getElementById('hla-dp');
+  const hlaDQ = document.getElementById('hla-dq');
+  const hlaDR = document.getElementById('hla-dr');
+  const other = document.getElementById('other');
 
-// Throwing the API url in a variable:
-const requestURL = 'https://www.ebi.ac.uk/cgi-bin/ipd/api/allele?limit=1000&project=HLA';
-
-// Fetch request to the API for the allele data:
-fetch(requestURL)
-  // converting data to json:
-  .then((responseData) => {
-    return responseData.json();
-  })
-  .then((jsonData)=>{
-    console.log('Allele data:', jsonData.data);
-    let alleleData = jsonData.data;
-    // Console logs to make sure I'm accessing the specific part of the data I'm looking for:
-    console.log(alleleData[0].accession);
-    console.log(alleleData[0].name);
-
-   // For loop to loop through data array of objects to create separate divs for each allele accession/name pair:
-    for (let i = 0; i < alleleData.length; i++) {
-      let newAllele = document.createElement('div');
-      newAllele.classList.add('allele');
-
-      // Creating individual divs for each accession and name, and then appending both to new parent div:
-      let alleleAccession = document.createElement('div');
-      alleleAccession.classList.add('allele-element');
-      alleleAccession.innerHTML = alleleData[i].accession + ':';
-      newAllele.appendChild(alleleAccession)
-
-      let alleleName = document.createElement('div');
-      alleleName.classList.add('allele-element');
-      alleleName.innerHTML = alleleData[i].name;
-      newAllele.appendChild(alleleName)
-
-      // Conditional logic for sorting them by isotype:
-      if (alleleData[i].name.startsWith('A')) {
-        hlaA.appendChild(newAllele);
-    } else if (alleleData[i].name.startsWith('B')) {
-        hlaB.appendChild(newAllele);
-    } else if (alleleData[i].name.startsWith('C')) {
-        hlaC.appendChild(newAllele);
-    } else if (alleleData[i].name.startsWith('DP')) {
-        hlaDP.appendChild(newAllele);
-    } else if (alleleData[i].name.startsWith('DQ')) {
-        hlaDQ.appendChild(newAllele);
-    } else if (alleleData[i].name.startsWith('DR')) {
-        hlaDR.appendChild(newAllele);
-    } else {
-        other.appendChild(newAllele);
-    }
+  // Function to create a separate parent div for each allele:
+  const alleleDivCreator = () => {
+    let newAllele = document.createElement('div');
+    newAllele.classList.add('allele');
+    return newAllele;
   }
+
+  // Function to create separate child divs for both accession and name:
+  const alleleElementDivCreator = (parentDiv) => {
+    let alleleElement = document.createElement('div');
+    alleleElement.classList.add('allele-element');
+    parentDiv.appendChild(alleleElement)
+    return alleleElement;
+  }
+
+  // Function to conditionally sort data for display based on isotype:
+  const isotypeSorter = (alleleToSort, alleleString) => {
+
+    const strLowerCase = alleleString.toLowerCase();
+        
+    if (strLowerCase.startsWith('a')) {
+        hlaA.appendChild(alleleToSort);
+    } else if (strLowerCase.startsWith('b')) {
+        hlaB.appendChild(alleleToSort);
+    } else if (strLowerCase.startsWith('c')) {
+        hlaC.appendChild(alleleToSort);
+    } else if (strLowerCase.startsWith('dp')) {
+        hlaDP.appendChild(alleleToSort);
+    } else if (strLowerCase.startsWith('dq')) {
+        hlaDQ.appendChild(alleleToSort);
+    } else if (strLowerCase.startsWith('dr')) {
+        hlaDR.appendChild(alleleToSort);
+    } else {
+        other.appendChild(alleleToSort);
+    }
+
+  }
+
+  // Creating a variable for the API url:
+  const requestURL = 'https://www.ebi.ac.uk/cgi-bin/ipd/api/allele?limit=1000&project=HLA';
+
+  // Fetch request to the API for the allele data:
+  fetch(requestURL)
+
+    // Converting the data to json:
+    .then((responseData) => {
+      return responseData.json();
+    })
+
+    // Creating a variable for all json data:
+    .then((jsonData)=>{
+      // Initial console log to see the structure of data received:
+      console.log('Allele data:', jsonData.data);
+      let alleleData = jsonData.data;
+      // Console logs to ensure I'm accessing the specific part of data I'm looking for:
+      console.log(alleleData[0].accession);
+      console.log(alleleData[0].name);
+
+    // For loop, to loop through each data object and create separate divs for each allele accession/name pair and then sort them based on isotype:
+      for (let i = 0; i < alleleData.length; i++) {
+        
+        // Creating parent div:
+        const newParentDiv = alleleDivCreator()
+        
+        // Creating both child divs, one for accession and one for name:
+        const newAlleleAccession = alleleElementDivCreator(newParentDiv);
+        newAlleleAccession.innerHTML = alleleData[i].accession + ':';
+
+        const newAlleleName = alleleElementDivCreator(newParentDiv);
+        newAlleleName.innerHTML = alleleData[i].name;
+        const alleleString = newAlleleName.innerHTML
+
+        // Sorting them for display based on isotype:
+        isotypeSorter(newParentDiv, alleleString);
+
+      }
+
+    })
+
+    // Error handling function, in case something goes wrong with the main API request:
+    .catch((error) => {
+      console.log('Error fetching data from API', error);
+    })
+
   })
-  // Error catch function, in case something goes wrong with the main API request:
-  .catch((error) => {
-    console.log('Error fetching data from API', error);
-  })
-})
