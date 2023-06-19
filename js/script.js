66f1a8512cea7da9d36346ccf6f6798391345179
emilyjryan <emilyryanmac@gmail.com>
2023-06-19 13:22:35 -0700

    cleaned up code, edited for typos

--- js/script.js
+++ js/script.js
@@ -4,10 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('JS file loaded and running')
 
   // Defining variables from html elements:
-  const main = document.getElementById('main-header');
   const loadingDiv = document.getElementById('loading');
   const hlaA = document.getElementById('hla-a');
-  const hlaHeader = document.getElementById('hla-a-header')
   const hlaB = document.getElementById('hla-b');
   const hlaC = document.getElementById('hla-c');
   const hlaDP = document.getElementById('hla-dp');
@@ -21,9 +19,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const allColumns = [hlaA, hlaB, hlaC, hlaDP, hlaDQ, hlaDR, other]
 
 
-// == SEARCH FOR SPECIFIC SEQUENCE == //
+  // == SEARCH FOR SPECIFIC SEQUENCE == //
 
-// Create array with only 'name' entries from main data array:
+  // Create array with only 'name' entries from main data array:
   const allNamesArray = (data) => {
     let namesArray = [];
     for (let i = 0; i < data.length; i++) {
@@ -32,25 +30,23 @@ document.addEventListener('DOMContentLoaded', () => {
     return namesArray 
   }
 
-// Search loop functionality:
+  // Search loop functionality:
   const searchLoop = (searchValue, allData) => {
     const nameArray = allNamesArray(allData)
     let match = false;
     for (let i = 0; i < nameArray.length; i++) {
       if (searchValue === nameArray[i]) {
-        console.log('match', nameArray[i])
         match = true;
         return i
       }
     }
     if (match === false) {
-      console.log('no match')
       results.style.display = 'block';
       results.innerHTML = 'No results found'
     }
   }
 
-// == DISPLAYING DATA FROM API == //
+  // == DISPLAYING DATA FROM API == //
 
   // Function to create a separate parent div for each allele:
   const alleleDivCreator = () => {
@@ -113,68 +109,59 @@ document.addEventListener('DOMContentLoaded', () => {
   // Fetch request to the API for the allele data:
   fetch(requestURL)
 
-    // Converting the data to json:
-    .then((responseData) => {
-      return responseData.json();
-    })
+  // Converting the data to json:
+  .then((responseData) => {
+    return responseData.json();
+  })
 
-    // Creating a variable for all json data:
-    .then((jsonData)=>{
-      // Initial console log to see the structure of data received:
-      console.log('Allele data:', jsonData.data);
-      let alleleData = jsonData.data;
-      // Console logs to ensure I'm accessing the specific part of data I'm looking for:
-      console.log(alleleData[0].accession);
-      console.log(alleleData[0].name);
+  // Creating a variable for all json data:
+  .then((jsonData)=>{
+    let alleleData = jsonData.data;
 
-      loading = false;
+    loading = false;
 
     // For loop, to loop through each data object and create separate divs for each allele accession/name pair and then sort them based on isotype:
-      if (loading === false) {
-       allAlleles.style.display = 'block';
-       loadingDiv.style.display = 'none';
+    if (loading === false) {
+      allAlleles.style.display = 'block';
+      loadingDiv.style.display = 'none';
+    }
 
-        for (let i = 0; i < alleleData.length; i++) {
-          
-          // Creating parent div:
-          const newParentDiv = alleleDivCreator()
-          
-          // Creating both child divs, one for accession and one for name:
-          const newAlleleAccession = alleleElementDivCreator(newParentDiv);
-          newAlleleAccession.innerHTML = alleleData[i].accession + ':';
+    for (let i = 0; i < alleleData.length; i++) {
+        
+      // Creating parent div:
+      const newParentDiv = alleleDivCreator()
+      
+      // Creating both child divs, one for accession and one for name:
+      const newAlleleAccession = alleleElementDivCreator(newParentDiv);
+      newAlleleAccession.innerHTML = alleleData[i].accession + ':';
+      const newAlleleName = alleleElementDivCreator(newParentDiv);
+      const alleleString = newAlleleName.innerHTML += alleleData[i].name;
 
-          const newAlleleName = alleleElementDivCreator(newParentDiv);
-          const alleleString = newAlleleName.innerHTML += alleleData[i].name;
-
-          // Sorting them for display based on isotype:
-          isotypeSorter(newParentDiv, alleleString);
-
-          
-        }
-
-        // Displaying total entries beneath isotype columns:
-        allColumns.forEach(column => entryCounter(column))
-
-        // Search button functionality:
-        searchBtn.addEventListener('click', () => {
-          results.innerHTML = 'Search results: '
-          let searchValue = searchBox.value;
-          console.log('search value', searchValue)
-          let searchMatchIndex = searchLoop(searchValue, alleleData)
-          let matchAccession = alleleData[searchMatchIndex].accession;
-
-          // Display div with match pair:
-          results.innerHTML += matchAccession += ' = ' + searchValue;
-          results.style.display = 'block';
-        })
+      // Sorting them for display based on isotype:
+      isotypeSorter(newParentDiv, alleleString);
 
     }
 
-    })
+    // Displaying total entries beneath isotype columns:
+    allColumns.forEach(column => entryCounter(column))
 
-    // Error handling function, in case something goes wrong with the main API request:
-    .catch((error) => {
-      console.log('Error fetching data from API', error);
+    // Search button functionality:
+    searchBtn.addEventListener('click', () => {
+      results.innerHTML = 'Search results: '
+      let searchValue = searchBox.value;
+      let searchMatchIndex = searchLoop(searchValue, alleleData)
+      let matchAccession = alleleData[searchMatchIndex].accession;
+
+      // Display div with match pair:
+      results.innerHTML += matchAccession += ' = ' + searchValue;
+      results.style.display = 'block';
     })
 
   })
+
+  // Error handling function, in case something goes wrong with the main API request:
+  .catch((error) => {
+    console.log('Error fetching data from API', error);
+  })
+
+})
